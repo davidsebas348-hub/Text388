26cac57506c6218cf4388f8e6d3f4b25d8a601ce
davidsebas348-hub <davidsebas348@gmail.com>
2026-03-22 16:17:26 -0500

    Update Text388.lua

--- Text388.lua
+++ Text388.lua
@@ -1,22 +1,17 @@
+-- TOGGLE
+getgenv().NPC_HIGHLIGHT = not getgenv().NPC_HIGHLIGHT
+if not getgenv().NPC_HIGHLIGHT then
+	for _,v in pairs(workspace:GetDescendants()) do
+		if v.Name == "ESP_HIGHLIGHT" then
+			v:Destroy()
+		end
+	end
+	return
+end
+
 repeat task.wait() until game:IsLoaded()
 
 local Enemies = workspace:WaitForChild("Enemies")
-
--- TOGGLE GLOBAL
-if _G.ESP_NPC_ON == nil then
-    _G.ESP_NPC_ON = false
-end
-
-_G.ESP_NPC_ON = not _G.ESP_NPC_ON
-
--- conexiones
-if not _G.ESP_NPC_CONNS then
-    _G.ESP_NPC_CONNS = {}
-end
-
---------------------------------------------------
--- COLORES
---------------------------------------------------
 
 local function getColor(name)
 	name = string.lower(name)
@@ -34,70 +29,25 @@ local function getColor(name)
 	end
 end
 
---------------------------------------------------
--- CREAR ESP
---------------------------------------------------
+local function create(npc)
+	if not getgenv().NPC_HIGHLIGHT then return end
+	if npc:FindFirstChild("ESP_HIGHLIGHT") then return end
 
-local function create(NPC)
-	if not _G.ESP_NPC_ON then return end
-	if not NPC or not NPC:IsA("Model") then return end
-
-	if NPC:FindFirstChild("ESP_HIGHLIGHT") then return end
-	
 	local h = Instance.new("Highlight")
 	h.Name = "ESP_HIGHLIGHT"
-	h.FillColor = getColor(NPC.Name)
+	h.FillColor = getColor(npc.Name)
 	h.OutlineColor = h.FillColor
 	h.FillTransparency = 0.5
 	h.OutlineTransparency = 0
-	h.Adornee = NPC
-	h.Parent = NPC -- 🔥 ahora va dentro del NPC
+	h.Adornee = npc
+	h.Parent = npc
 end
 
---------------------------------------------------
--- ACTIVAR
---------------------------------------------------
-
-if _G.ESP_NPC_ON then
-
-	print("ESP NPC ACTIVADO")
-
-	-- existentes
-	for _,v in ipairs(Enemies:GetChildren()) do
-		create(v)
-	end
-
-	-- nuevos
-	table.insert(_G.ESP_NPC_CONNS,
-		Enemies.ChildAdded:Connect(function(v)
-			task.wait(0.2)
-			create(v)
-		end)
-	)
-
---------------------------------------------------
--- DESACTIVAR
---------------------------------------------------
-
-else
-
-	print("ESP NPC DESACTIVADO")
-
-	-- desconectar todo
-	for _,c in pairs(_G.ESP_NPC_CONNS) do
-		pcall(function()
-			c:Disconnect()
-		end)
-	end
-
-	_G.ESP_NPC_CONNS = {}
-
-	-- borrar highlights
-	for _,v in ipairs(Enemies:GetChildren()) do
-		local h = v:FindFirstChild("ESP_HIGHLIGHT")
-		if h then
-			h:Destroy()
-		end
-	end
-
+for _,v in ipairs(Enemies:GetChildren()) do
+	create(v)
 end
+
+Enemies.ChildAdded:Connect(function(v)
+	task.wait(0.2)
+	create(v)
+end)
